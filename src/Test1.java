10559e26f6b9399f7d5e94f5d675f6689e1ba46b
Whynn <fine612@gmail.com>
2016-04-12 11:36:21 +0900

    added new print line message

--- src/Test1.java
+++ src/Test1.java
@@ -2,5 +2,6 @@
 public class Test1 {
 	public static void main(String[] argv){
 		System.out.println("Hello World!!");
+		System.out.println("My first test to connect with eclipse and github!!");
 	}
 }
