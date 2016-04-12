4958984ee053bd63081582cf782a99622a7d1b78
Whynn <fine612@gmail.com>
2016-04-12 11:50:56 +0900

    add function f

--- src/Test1.java
+++ src/Test1.java
@@ -4,4 +4,11 @@ public class Test1 {
 		System.out.println("Hello World!!");
 		System.out.println("My first test to connect with eclipse and github!!");
 	}
+	public void f(){
+		int sum=0;
+		for(int i=0 ; i < 5 ; i++){
+			sum += i;
+		}
+		System.out.println(sum);
+	}
 }
